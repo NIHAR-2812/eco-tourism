c73d34f1633372c76e8834ca59ef6a77569bae15
Swaraj <swarajdalvi0@gmail.com>
2026-08-20 01:43:25 +0530

    db function

--- backend/app/database.py
+++ backend/app/database.py
@@ -26,3 +26,9 @@ else:
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
